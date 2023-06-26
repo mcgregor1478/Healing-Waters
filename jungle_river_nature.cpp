75e625449cffe4f2cfbccdb6b4f9cf03a7740643
mcgregor1478 <136748696+mcgregor1478@users.noreply.github.com>
2023-06-27 05:11:04 +0700

    Fix input sanitization vulnerabilities

---
+++ jungle_river_nature.cpp
@@ -0,0 +1,53 @@
+song
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	// Verse 1
+	cout << "Beautiful Savior, King of Creation" << endl;
+	cout << "You are my God and Redeemer" << endl;
+	cout << "I exalt Your name evermore" << endl;
+	cout << "Beautiful Savior, Lord of my life" << endl << endl;
+	
+	// Chorus
+	cout << "Healing waters, open up for me" << endl;
+	cout << "Open up for me and I will be" << endl;
+	cout << "Free, free, free, free" << endl;
+	cout << "Healing waters, overpower me" << endl;
+	cout << "Overpower me and I will be" << endl;
+	cout << "Free, free, free, free" << endl << endl;
+	
+	// Verse 2
+	cout << "My constant longing and my need" << endl;
+	cout << "My helpless hunger and my greed" << endl;
+	cout << "My failures and my shame" << endl;
+	cout << "In search of love I come again" << endl << endl;
+	
+	// Chorus
+	cout << "Healing waters, open up for me" << endl;
+	cout << "Open up for me and I will be" << endl;
+	cout << "Free, free, free, free" << endl;
+	cout << "Healing waters, overpower me" << endl;
+	cout << "Overpower me and I will be" << endl;
+	cout << "Free, free, free, free" << endl << endl;
+	
+	// Bridge
+	cout << "Replace my fears with perfect trust" << endl;
+	cout << "Replace my doubts with perfect love" << endl;
+	cout << "Replace my shame and brokenness" << endl;
+	cout << "With Your sweet promises of peace" << endl << endl;
+	
+	// Chorus
+	cout << "Healing waters, open up for me" << endl;
+	cout << "Open up for me and I will be" << endl;
+	cout << "Free, free, free, free" << endl;
+	cout << "Healing waters, overpower me" << endl;
+	cout << "Overpower me and I will be" << endl;
+	cout << "Free, free, free, free" << endl;
+
+	return 0;
+}
